afc8a3a8fea284417204fec3f5abc4b4b64fbcdf
jnolascob <jnolascob@gmail.com>
2014-11-28 00:53:47 -0500

    Update util fonts

--- app/src/main/java/me/doapps/christmasrecipes/utils/UtilFonts.java
+++ app/src/main/java/me/doapps/christmasrecipes/utils/UtilFonts.java
@@ -10,4 +10,16 @@ public class UtilFonts {
     public static Typeface setHollyBerry(Context context){
         return Typeface.createFromAsset(context.getAssets(), "fonts/HollyBerryWonderland.ttf");
     }
+    public static Typeface setHelveticaBold(Context context){
+        return Typeface.createFromAsset(context.getAssets(), "fonts/Helvetica_Bold.otf.ttf");
+    }
+    public static Typeface setHelveticaMedium(Context context){
+        return Typeface.createFromAsset(context.getAssets(), "fonts/Helvetica_Medium.otf.ttf");
+    }
+    public static Typeface setHelveticaRoman(Context context){
+        return Typeface.createFromAsset(context.getAssets(), "fonts/Helvetica_Roman.otf.ttf");
+    }
+    public static Typeface setHelveticaThin(Context context){
+        return Typeface.createFromAsset(context.getAssets(), "fonts/Helvetica_Thin.otf.ttf");
+    }
 }
